c38615fdc76491e3580cd58abcff34a9416a491b
ZTF <ZTF666@protonmail.ch>
2020-05-27 18:25:50 +0000

    :heart:

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app dark>
+  
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
